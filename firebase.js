b872fea021d1dacf56657981b38378447950cb6b
Gaurav-Soni24 <sonigaurav2021@gmail.com>
2024-10-01 20:51:02 +0530

    updated backend

--- firebase.js
+++ firebase.js
@@ -18,30 +18,32 @@ let app;
 let firestoreDb;
 
 const initializeFirebase = () => {
-  try {
-    const serviceAccount = {
-      type: FIREBASE_SERVICE_ACCOUNT_TYPE,
-      project_id: FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
-      private_key_id: FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-      private_key: FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
-      client_email: FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
-      client_id: FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
-      auth_uri: FIREBASE_SERVICE_ACCOUNT_AUTH_URI,
-      token_uri: FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
-      auth_provider_x509_cert_url: FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
-      universe_domain: FIREBASE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN
-    };
-    app = admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount)
-    });
-    firestoreDb = admin.firestore();
-    console.log("Firebase initialized successfully");
-    return app;
-  } catch (error) {
-    console.error("Error initializing Firebase:", error);
-    return null; // Return null if initialization fails
+  if (!app) {
+    try {
+      const serviceAccount = {
+        type: FIREBASE_SERVICE_ACCOUNT_TYPE,
+        project_id: FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
+        private_key_id: FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+        private_key: FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+        client_id: FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
+        auth_uri: FIREBASE_SERVICE_ACCOUNT_AUTH_URI,
+        token_uri: FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
+        auth_provider_x509_cert_url: FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+        universe_domain: FIREBASE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN
+      };
+      app = admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+      });
+      firestoreDb = admin.firestore();
+      console.log("Firebase initialized successfully");
+    } catch (error) {
+      console.error("Error initializing Firebase:", error);
+      throw error;
+    }
   }
+  return app;
 };
 
 const uploadProcessData = async (collectionName, documentId, data) => {
